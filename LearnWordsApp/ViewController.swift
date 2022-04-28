4b9e72a94c5489ef919a95f5bba9211d3244a6d9
KonstantinKnor <knorkostya44@gmail.com>
2022-04-28 14:44:01 +0300

    Initial Commit

---
+++ LearnWordsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnWordsApp
+//
+//  Created by Константин Кнор on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
